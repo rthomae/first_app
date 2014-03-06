a8403d46a658ea7fa71cdefecc0fbf91f28f1178
rthomae <slick@alum.mit.edu>
2014-03-06 10:27:00 -0700

    init repo, redux - deleted dir and made again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '8dd08e0b516913399a3d7ded6e91f8cbd1e010fd6e34df2f6c0395ffd6da0a250470e3722b3141996f5afd96eaa10e180622e99c3c30fec5225469880b5a15eb'
